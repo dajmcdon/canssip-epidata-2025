905470d846054bacb87f59da6e9a847302566e4a
Daniel J. McDonald <dajmcdon@gmail.com>
2024-11-10 16:15:05 -0800

    switch to dark blue + some dark red

--- _code/cover-image.R
+++ _code/cover-image.R
@@ -1,8 +1,8 @@
 library(epiprocess)
 library(tidyverse)
-primary <- "#a8201a"
+primary <- "#941120"
 x <- archive_cases_dv_subset
-x_latest <- epix_as_of(x, version = max(x$DT$version))
+x_latest <- epix_as_of(x, max_version = max(x$DT$version))
 self_max = max(x$DT$version)
 versions = seq(as.Date("2020-06-01"), self_max - 1, by = "1 month")
 snapshots_all <- map_dfr(versions, function(v) {
@@ -23,14 +23,18 @@ snapshots_all |>
   scale_x_date(breaks = as.Date(c("2021-01-01", "2021-06-01")),
                expand = expansion(0),
                date_labels = "%B %Y") +
-  labs(x = "", y = "% Outpatient visits due to COVID-like\nillness in CA and FL") +
+  labs(x = "", y = "") +
+  #labs(x = "", y = "% Outpatient visits due to COVID-like\nillness in CA and FL") +
   theme_bw() +
   coord_cartesian(xlim = as.Date(c("2020-10-01", "2021-10-01")), ylim = c(0, NA)) +
   scale_color_viridis_d(option = "B", end = .8) +
-  theme(legend.position = "none", panel.background = element_blank()) +
+  theme(
+    legend.position = "none", panel.background = element_blank(),
+    axis.text = element_blank()
+  ) +
   geom_line(
     data = snapshots %>% filter(latest),
     aes(x = time_value, y = percent_cli, group = geo_value),
     inherit.aes = FALSE, color = primary)
 
-ggsave("assets/img/cover-art.svg", width = 6, height = 6)
+ggsave("assets/img/cover-art.svg", width = 8, height = 4)
